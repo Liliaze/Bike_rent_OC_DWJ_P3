3f66e1cd68fda13d8750d9cedd6b2eeb07795e1f
Diana Boudy <diana.boudy@gmail.com>
2018-10-24 14:10:38 +0200

    prise en charge signature mobile

--- js/Canvas.js
+++ js/Canvas.js
@@ -1,13 +1,14 @@
 var Canvas = {
-    init : function(canvasDivId) {
+    init: function (canvasDivId) {
         cthis = this;
         cthis.canvasElt = this.addCanvasInDIv(canvasDivId);
         cthis.ctx = this.canvasElt.getContext('2d');
         cthis.imagePencil = new Image();
         cthis.imagePencil.onload = e => cthis.ctx.drawImage(cthis.imagePencil, 5, 5, 50, 50);
-        cthis.ctx.font="14px Georgia";
-        cthis.ctx.fillText("Signature!",5,72);
-        cthis.imagePencil.src = "./img/pencil.png";;
+        cthis.ctx.font = "14px Georgia";
+        cthis.ctx.fillText("Signature!", 5, 72);
+        cthis.imagePencil.src = "./img/pencil.png";
+        ;
         cthis.isDrawing = false;
         cthis.eventCanvas();
         //document.body.onload = function (){cthis.startUp();};
@@ -21,15 +22,19 @@ var Canvas = {
         return canvasElt;
     },
     eventCanvas: function () {
+        cthis.canvasElt.addEventListener("touchstart", cthis.canvasTouchStart);
+        cthis.canvasElt.addEventListener("touchmove", cthis.canvasTouchMove);
+        cthis.canvasElt.addEventListener("touchend", cthis.canvasDrawStop);
+        cthis.canvasElt.addEventListener("touchleave", cthis.canvasDrawStop);
         cthis.canvasElt.addEventListener("mousedown", cthis.canvasDrawStart);
         cthis.canvasElt.addEventListener("mousemove", cthis.canvasDrawing);
         cthis.canvasElt.addEventListener("mouseup", cthis.canvasDrawStop);
         cthis.canvasElt.addEventListener("mouseout", cthis.canvasDrawStop);
     },
-    canvasDrawStart: function(e) {
-            cthis.isDrawing = true;
-            cthis.ctx.moveTo((e.pageX - cthis.canvasElt.offsetLeft), (e.pageY - cthis.canvasElt.offsetTop));
-        },
+    canvasDrawStart: function (e) {
+        cthis.isDrawing = true;
+        cthis.ctx.moveTo((e.pageX - cthis.canvasElt.offsetLeft), (e.pageY - cthis.canvasElt.offsetTop));
+    },
     canvasDrawing: function (e) {
         if (cthis.isDrawing) {
             cthis.ctx.lineTo((e.pageX - cthis.canvasElt.offsetLeft), (e.pageY - cthis.canvasElt.offsetTop));
@@ -39,13 +44,15 @@ var Canvas = {
     canvasDrawStop: function () {
         cthis.isDrawing = false;
     },
-    //GESTION TOUCH SUR MOBILE
-    startUp: function() {
-        console.log("startevent ===");
-        cthis.canvasElt.addEventListener("touchstart", cthis.handleStart, false);
-        cthis.canvasElt.addEventListener("touchmove", cthis.handleMove, false);
-        cthis.canvasElt.addEventListener("touchend", cthis.handleEnd, false);
-        cthis.canvasElt.addEventListener("touchleave", cthis.handleEnd, false);
-        cthis.canvasElt.addEventListener("touchcancel", cthis.handleCancel, false);
+    // Set up touch events for mobile, etc
+    canvasTouchStart: function (e) {
+        cthis.isDrawing = true;
+        cthis.ctx.moveTo((e.touches[0].clientX - cthis.canvasElt.offsetLeft), (e.touches[0].clientY - cthis.canvasElt.offsetTop));
+    },
+    canvasTouchMove: function (e) {
+        if (cthis.isDrawing) {
+            cthis.ctx.lineTo((e.touches[0].clientX - cthis.canvasElt.offsetLeft), (e.touches[0].clientY - cthis.canvasElt.offsetTop));
+            cthis.ctx.stroke();
+        }
     }
 };
